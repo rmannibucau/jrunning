a97d99fb884634e1cb8f89f1bad5af8fbb23788c
Romain Manni-Bucau <rmannibucau@apache.org>
2015-04-23 21:03:41 +0200

    let's kill ejbs

--- jrunning-server/src/test/java/io/github/rmannibucau/jrunning/jaxrs/test/Tx.java
+++ jrunning-server/src/test/java/io/github/rmannibucau/jrunning/jaxrs/test/Tx.java
@@ -1,10 +1,12 @@
 package io.github.rmannibucau.jrunning.jaxrs.test;
 
 import java.util.concurrent.Callable;
-import javax.ejb.Singleton;
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
 
-@Singleton
+@ApplicationScoped
 public class Tx { // just a test helper to not worry about tx/catch
+    @Transactional
     public <T> T run(Callable<T> run) {
         try {
             return run.call();
